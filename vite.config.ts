8c245a24202a1545c1a848f2c92f7e3baf381fef
undownding <undownding@gmail.com>
2022-01-18 15:02:20 +0800

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  optimizeDeps: {
+    include: ['vue3-mobile-detection', 'is-mobile']
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  }
 })
